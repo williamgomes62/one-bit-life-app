9f6c8c098c6d1f7e286bdf5fe63dac4886975ed1
William Gomes <williamlimagomes62@gmail.com>
2022-11-29 15:49:43 -0300

    feat: create AppExplanation screen

---
+++ src/Pages/AppExplanation/index.jsx
@@ -0,0 +1,59 @@
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import DefaultButton from '../../Common/DefaultButton';
+
+export default function AppExplanation() {
+  function handleSetShowHome() {
+    alert('handle home');
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{alignItems: 'center'}}>
+          <Text style={styles.title}>
+            Antes, deixa {'\n'} eu te explicar...
+          </Text>
+          <Text style={styles.descriptionCta}>
+            Pronto(a) para subir de nível na vida?
+          </Text>
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {'\n'}
+            seus 4 hábitos de forma individual.
+          </Text>
+          <DefaultButton
+            buttonText={'Continuar'}
+            handlePress={handleSetShowHome}
+            width={250}
+            height={50}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(21, 21, 21, 0.98)',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginVertical: 40,
+  },
+  descriptionCta: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  description: {
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 30,
+  }
+});
