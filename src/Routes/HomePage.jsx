e6f7eefddff20d1ff27092b89ea10f7e06b50dc6
William Gomes <williamlimagomes62@gmail.com>
2022-12-08 10:32:24 -0300

    feat: routes structure and HomePage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,7 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Start from '../Pages/Start';
 import AppExplanation from '../Pages/AppExplanation';
 import Home from '../Pages/Home';
 import HabitPage from '../Pages/HabitPage';
@@ -16,7 +14,6 @@ export default function AllPages() {
           headerShown: false
         }}
       >
-        <Stack.Screen name='Start' component={Start} />
         <Stack.Screen name='AppExplanation' component={AppExplanation} />
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='HabitPage' component={HabitPage} />
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import AllPages from './AllPages';
+import HomePage from './HomePage';
 
 export default function Routes() {
+  const [showHome, setShowHome] = useState(false);
+
   return (
-    <AllPages />
+    <>
+      {showHome ? (
+        <HomePage />
+      ) : (
+        <AllPages />
+      )}
+    </>
   );
 }
